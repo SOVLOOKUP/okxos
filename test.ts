1b3085c0fb411ec07dd832e10968ab321a0bf13c
sovlookup <gonorth@qq.com>
2024-11-28 16:13:26 +0800

    chain string

--- test.ts
+++ test.ts
@@ -1,3 +1,4 @@
+import { Chain } from "./src";
 import { apiKey, apiSecret, apiPass, projectID } from "./secret.json";
 import { SDK } from "./src";
 
@@ -10,17 +11,30 @@ const client = SDK({
 
 // console.log(
 //   await client({
-//     endpoint: "wallet/chain/supported-chains",
+//     endpoint: "wallet/webhook/unsubscribe",
+//     params: [{ id: "200207" }],
 //   })
 // );
 
 console.log(
   await client({
-    endpoint: "wallet/webhook/unsubscribe",
+    endpoint: "wallet/webhook/subscribe",
     params: [
       {
-        id: "200204",
+        type: "fee_fluctuation",
+        chainIndex: Chain.Ethereum,
+        url: "https://webhook.site/ff8fac2d-72df-40a4-ae39-6ee244657992",
+        name: "fee",
+        feeChangeFilter: {
+          minChange: 0.01
+        }
       },
     ],
+  })
+);
+
+console.log(
+  await client({
+    endpoint: "wallet/webhook/subscriptions",
   })
 );
